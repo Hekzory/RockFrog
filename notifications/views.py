95454a59e2d79276bc317757243632c118e45fac
Alehandreus <alehandro.nikolaev@gmail.com>
2019-10-08 22:56:21 +0300

    Notifications page Update
    Now unchecked notifications are shown at the top of the page despite their date

--- notifications/views.py
+++ notifications/views.py
@@ -11,32 +11,32 @@ class NotificationsList(View):
     def get(self, request):
         if request.user.is_authenticated:
             page = self.request.GET.get('page')
-            all_nots = Notificationlist.objects.all().filter(user = request.user)[0].notifications.all()[::-1]
-            paginator = Paginator(all_nots, 4)
+            new_notifications = Notificationlist.objects.all().filter(user = request.user)[0].notifications.filter(not_checked = True)[::-1]
+            old_notifications = Notificationlist.objects.all().filter(user = request.user)[0].notifications.filter(not_checked = False)[::-1]
+            all_notifications = new_notifications + old_notifications
+
+            paginator = Paginator(all_notifications, 4)
 
             try:
-                nots = paginator.page(page)
+                notifications = paginator.page(page)
             except PageNotAnInteger:
                 # If page is not an integer, deliver first page.
-                nots = paginator.page(1)
+                notifications = paginator.page(1)
             except EmptyPage:
                 # If page is out of range (e.g. 9999), deliver last page of results.
-                nots = paginator.page(paginator.num_pages)
+                notifications = paginator.page(paginator.num_pages)
 
             template = loader.get_template('notifications/index.html')
             context = {
-                'notifications': nots,
+                'notifications': notifications,
             }
             return HttpResponse(template.render(context, request))
         else:
             return HttpResponseRedirect("/auth/login")
 
-    def post(request, not_id):
-        try:
-            notification = Notificationlist.objects.all().filter(user = request.user)[0].notifications.objects.get(id = not_id)
-            notification.not_checked = False
-            notification.save()
-        except:
-            pass
+    def post(self, request):
+        temp_id = int(request.POST.get('not_id', None))
+        notification = Notificationlist.objects.all().filter(user = request.user)[0].notifications.all().filter(id = temp_id)[0]
+        notification.not_checked = False
+        notification.save()
         return HttpResponseRedirect("/notifications/")
-
