43699f3cfe27471b0315f98fb37f54dcd5cd11c1
Avalon <55704040+AvalonAVL@users.noreply.github.com>
2019-10-11 16:50:18 +0300

    Profiles - 1st commit
    Добавлены поля "город", "телефон", "интересы" в модель Profile.
    Кнопка на главной панели теперь ведёт на профиль авторизованного пользоватлея и показывает его никнейм. Работает как при нахождении на главной странице, так и при просмотре чужого или своего профиля

--- UserProfile/views.py
+++ UserProfile/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.template import loader
 from django.contrib.auth.models import User
