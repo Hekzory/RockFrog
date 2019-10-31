e6bc6a52310ae98f242aa586f949a16b53be2389
Avalon <55704040+AvalonAVL@users.noreply.github.com>
2019-10-31 18:56:50 +0300

    First version of profile editing

--- UserProfile/forms.py
+++ UserProfile/forms.py
@@ -2,16 +2,42 @@ from django import forms
 from .models import Profile
 from django.core.exceptions import ValidationError
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['user', 'about', 'birth_date', 'email', 'city', 'phone', 'interests']
-        widgets = {
-            'user' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'about' : forms.Textarea(attrs={'class' : 'form-control'}),
-            'birth_date' : forms.DateInput(attrs={'class' : 'form-control'}),
-            'email' : forms.EmailInput(attrs={'class' : 'form-control'}),
-            'city' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'phone' : forms.NumberInput(attrs={'class' : 'form-control'}),
-            'interests' : forms.Textarea(attrs={'class' : 'form-control'}),
-        }
+class ProfileForm(forms.Form):
+    about = forms.CharField(max_length=500)
+    birth_date = forms.DateField(widget=forms.DateInput())
+    email = forms.CharField(max_length=50)
+    city = forms.CharField(max_length=30)
+    phone = forms.CharField(max_length=30)
+    interests = forms.CharField(max_length=500)
+
+    #Сделать нормальные типы field
+
+    def get_about(self):
+        return self.cleaned_data['about']
+
+    def get_birth_date(self):
+        return self.cleaned_data['birth_date']
+
+    def get_email(self):
+        return self.cleaned_data['email']
+
+    def get_city(self):
+        return self.cleaned_data['city']
+
+    def get_phone(self):
+        return self.cleaned_data['phone']
+
+    def get_interests(self):
+        return self.cleaned_data['interests']
+
+    #Написать clean-ы
+
+    def change_profile(self, user):
+        profile = user.profile
+        profile.about = get_about()
+        profile.birth_date = get_birth_date()
+        profile.email = get_email()
+        profile.city = get_city()
+        profile.phone = get_phone()
+        profile.interests = get_interests()
+        profile.save()
--- UserProfile/views.py
+++ UserProfile/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.template import loader
 from django.contrib.auth.models import User
-
+from django.views.generic import View
+from .forms import ProfileForm
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
@@ -29,3 +30,32 @@ def userprofile(request, username):
             return HttpResponseNotFound("User not found")
         context = {'user' : user}
         return HttpResponse(template.render(context, request))
+
+class ProfileView(View):
+
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect("/")
+        else:
+            template = loader.get_template('UserProfile/yourprofile.html')
+            form = ProfileForm()
+            context = {'form' : form}
+            return HttpResponse(template.render(context, request))
+
+    def post(self, request):
+        if not request.user.is_authenticated:
+            print('asdasasdafasfasfasfafasgfjhg')
+            return HttpResponseRedirect("/")
+        else:
+            bound_form = ProfileForm(request.POST)
+            if bound_form.is_valid():
+                bound_form.change_profile(request.user)
+                template = loader.get_template('UserProfile/yourprofile.html')
+                form = ProfileForm()
+                context = {'form' : form}
+                return HttpResponse(template.render(context, request))
+            else:
+                template = loader.get_template('UserProfile/yourprofile.html')
+                form = ProfileForm()
+                context = {'form' : form}
+                return HttpResponse(template.render(context, request))
