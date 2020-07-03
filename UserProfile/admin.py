4e9b2ee9af58fdd52760fd3584b9bc11db3ae63e
AvalonAVL <sherstov2004_30@rambler.ru>
2020-07-03 22:06:27 +0300

    Notification settings page added

--- UserProfile/admin.py
+++ UserProfile/admin.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, PrivacySettings
+from .models import Profile, PrivacySettings, NotificationSettings
+
 
 class ProfilesAdmin(admin.ModelAdmin):
     fields = ['user', 'about', 'birth_date', 'avatar', 'blacklist', 'verified', 'last_time_online']
     readonly_fields = ['user']
     empty_value_display = 'Не указано'
 
+
 class PrivacySettingsAdmin(admin.ModelAdmin):
     fields = ['allow_to_view_for_unreg', 'profile']
 
 
+class NotificationSettingsAdmin(admin.ModelAdmin):
+    fields = ['personal_message_notifications', 'accepted_to_group_notifications', 'post_published_notifications',
+              'profile']
+
+
 admin.site.register(Profile, ProfilesAdmin)
 admin.site.register(PrivacySettings, PrivacySettingsAdmin)
+admin.site.register(NotificationSettings, NotificationSettingsAdmin)
--- UserProfile/urls.py
+++ UserProfile/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('blockedusers/', BlockedUsersView.as_view(), name='blockedusers'),
     path('edit_privacy/', EditPrivacyView.as_view(), name='edit_privacy'),
     path('edit_security/', EditSecurityView.as_view(), name='edit_security'),
+    path('edit_notifications/', EditNotificationsView.as_view(), name='edit_notifications'),
     path('block/', BlockUserView.as_view(), name='block_user'),
     path('unblock/', UnblockUserView.as_view(), name='unblock_user'),
     path('<str:username>/', UserProfileView.as_view(), name='userprofile'),
